a6ae79a475e8d08dff5c179dc5bb6a21b8c54c45
Rico Hancock <ricohancock@proton.me>
2025-11-06 15:04:51 -0700

    style: :art: Purple-ified and dates to end of november

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,7 +11,9 @@
 			'Wipe Countertops',
 			'Clean Stove',
 			'Clean Microwave',
-			'Sweep Kitchen Floor',
+			'Clear Out Old Food in Fridge',
+			'Sweep Floor',
+			'Mop Floor',
 			'Clear Dinner Table',
 			'Wipe Dinner Table'
 		],
@@ -28,7 +30,7 @@
 	const householdMembers = ['Rico', 'Melissa', 'Natalie', 'Ben'];
 
 	const startDay = new Date();
-	const daysToShow = 14;
+	const daysToShow = 24;
 	const days = eachDayOfInterval({ start: startDay, end: addDays(startDay, daysToShow) });
 	const choreRows =
 		Object.keys(chores).length +
@@ -40,12 +42,14 @@
 	<div class="flex w-full max-w-[120vh] items-center justify-center self-stretch">
 		<AspectRatio ratio={11 / 8.5} class="border">
 			<div class="flex h-full w-full">
-				<div class="grid grow grid-cols-11 grid-rows-8">
+				<div class="grid grow grid-cols-11 grid-rows-9">
 					<div
-						class="col-span-2 row-span-1 flex flex-col items-center justify-center border-r border-dashed"
+						class="col-span-2 row-span-1 flex flex-col items-center justify-center border-r-2 p-2"
 					>
-						<h1>Chore Logger</h1>
-						<p class="text-xs opacity-75">Initial the chores you do</p>
+						<h1 class="text-2xl font-bold text-violet-700">Chore Logger</h1>
+						<p class="text-center text-xs opacity-75">
+							Initial the chores you do on the date you do them
+						</p>
 					</div>
 					<div
 						style={`grid-template-columns: repeat(${days.length}, 1fr);`}
@@ -54,13 +58,19 @@
 						<!-- Dates -->
 						{#each days as day}
 							<div
-								class="relative flex w-full items-center justify-center border-r border-neutral-500 last:border-r-0"
+								class="relative flex w-full items-center justify-center border-r border-violet-500 last:border-r-0"
 							>
 								<div
-									class="absolute inset-0 flex -rotate-90 items-center justify-center text-xs text-nowrap"
+									class="absolute inset-0 flex flex-col items-center justify-center py-3 text-xs text-nowrap"
 								>
-									<p>
-										{format(day, 'ccc, MMM dd')}
+									<p class="mb-auto text-xs font-semibold uppercase opacity-50">
+										{format(day, 'MMM')}
+									</p>
+									<p class="text-sm font-semibold text-violet-700 uppercase">
+										{format(day, 'ccccc')}
+									</p>
+									<p class="text-xl font-semibold">
+										{format(day, 'd')}
 									</p>
 								</div>
 							</div>
@@ -68,33 +78,39 @@
 					</div>
 					<div
 						style={`grid-template-rows: repeat(${choreRows}, 1fr);`}
-						class="col-span-2 row-span-7 grid border-t border-r border-dashed"
+						class="col-span-2 row-span-8 grid border-t-2 border-r-2"
 					>
 						<!-- Chores -->
-						{#each Object.entries(chores) as [room, roomchores]}
-							<div class="flex items-center justify-center bg-neutral-500 text-white">
-								<h2 class="text-sm font-semibold capitalize">{room}</h2>
+						{#each Object.entries(chores) as [room, roomchores], index}
+							<div class="flex items-center justify-center bg-violet-500 text-white">
+								<h2 class="text-lg font-semibold tracking-wider uppercase">{room}</h2>
 							</div>
-							{#each roomchores as chore}
-								<div class="flex items-center border-b border-neutral-500 px-2 last:border-b-0">
-									<p class="w-full text-right text-xs">{chore}</p>
+							{#each roomchores as chore, choreIndex}
+								<div
+									data-row-even={(choreIndex + index - 1) % 2 === 0}
+									class="flex items-center border-b border-violet-500 px-2 last:border-b-0 data-[row-even='true']:bg-violet-100"
+								>
+									<p class="w-full text-right text-xs font-semibold">{chore}</p>
 								</div>
 							{/each}
 						{/each}
 					</div>
 					<div
 						style={`grid-template-rows: repeat(${choreRows}, 1fr); grid-template-columns: repeat(${days.length}, 1fr);`}
-						class="col-span-9 row-span-7 grid w-full border-t border-dashed"
+						class="col-span-9 row-span-8 grid w-full border-t-2"
 					>
 						<!-- Initial boxes -->
 						{#each initialBoxes as box, index}
-							{@const isLastBoxInRow = index % days.length === days.length - 1}
-							{@const isLastBoxInColumn = Math.floor(index / days.length) === choreRows - 1}
+							{@const row = Math.floor(index / days.length)}
+							{@const col = index % days.length}
+							{@const isLastBoxInRow = col === days.length - 1}
+							{@const isLastBoxInColumn = row === choreRows - 1}
 							<div
 								data-last-cell-in-row={isLastBoxInRow}
 								data-last-cell-in-column={isLastBoxInColumn}
+								data-row-even={row % 2 === 0}
 								style={`grid-template-columns: repeat(${days.length}, 1fr);`}
-								class="flex flex-col items-center justify-center border-r border-b border-neutral-500 px-2 text-xs data-[last-cell-in-column='true']:border-b-0 data-[last-cell-in-row='true']:border-r-0"
+								class="flex flex-col items-center justify-center border-r border-b border-violet-500 px-2 text-xs data-[last-cell-in-column='true']:border-b-0 data-[last-cell-in-row='true']:border-r-0 data-[row-even='true']:bg-violet-100"
 							></div>
 						{/each}
 					</div>
