584c1e9075209e1039ed585ab2aa6eb8b15a724d
Rico Hancock <ricohancock@proton.me>
2026-01-06 14:32:35 -0700

    chore: :memo: Update chores and date

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,30 +7,32 @@
 			'Wash Dishes',
 			'Load Dishwasher',
 			'Empty Dishwasher',
-			'Clear Countertops',
-			'Wipe Countertops',
 			'Clean Stove',
 			'Clean Microwave',
 			'Clear Out Old Food in Fridge',
 			'Sweep Floor',
 			'Mop Floor',
+			'Clear Countertops',
+			'Wipe Countertops',
 			'Clear Dinner Table',
-			'Wipe Dinner Table'
+			'Wipe Dinner Table',
+			'Take Out Trash'
 		],
-		livingroom: [
-			'Put Away Electronics',
-			'Vacuum Couch',
-			'Sweep Floor',
-			'Mop Floor',
-			'Dust Media Center',
-			'Dust TV',
-			'Wipe TV'
-		]
+		// livingroom: [
+		// 	'Put Away Electronics',
+		// 	'Vacuum Couch',
+		// 	'Sweep Floor',
+		// 	'Mop Floor',
+		// 	'Dust Media Center',
+		// 	'Dust TV',
+		// 	'Wipe TV'
+		// ]
+		outside: ['Picked Weeds', 'Watered Garden']
 	};
 	const householdMembers = ['Rico', 'Melissa', 'Natalie', 'Ben'];
 
-	const startDay = new Date();
-	const daysToShow = 24;
+	const startDay = new Date('2026-01-06');
+	const daysToShow = 26;
 	const days = eachDayOfInterval({ start: startDay, end: addDays(startDay, daysToShow) });
 	const choreRows =
 		Object.keys(chores).length +
@@ -46,7 +48,7 @@
 					<div
 						class="col-span-2 row-span-1 flex flex-col items-center justify-center border-r-2 p-2"
 					>
-						<h1 class="text-2xl font-bold text-violet-700">Chore Logger</h1>
+						<h1 class="text-2xl font-bold text-sky-700">Chore Logger</h1>
 						<p class="text-center text-xs opacity-75">
 							Initial the chores you do on the date you do them
 						</p>
@@ -58,7 +60,7 @@
 						<!-- Dates -->
 						{#each days as day}
 							<div
-								class="relative flex w-full items-center justify-center border-r border-violet-500 last:border-r-0"
+								class="relative flex w-full items-center justify-center border-r border-sky-500 last:border-r-0"
 							>
 								<div
 									class="absolute inset-0 flex flex-col items-center justify-center py-3 text-xs text-nowrap"
@@ -66,7 +68,7 @@
 									<p class="mb-auto text-xs font-semibold uppercase opacity-50">
 										{format(day, 'MMM')}
 									</p>
-									<p class="text-sm font-semibold text-violet-700 uppercase">
+									<p class="text-sm font-semibold text-sky-700 uppercase">
 										{format(day, 'ccccc')}
 									</p>
 									<p class="text-xl font-semibold">
@@ -82,13 +84,13 @@
 					>
 						<!-- Chores -->
 						{#each Object.entries(chores) as [room, roomchores], index}
-							<div class="flex items-center justify-center bg-violet-500 text-white">
+							<div class="flex items-center justify-center bg-sky-500 text-white">
 								<h2 class="text-lg font-semibold tracking-wider uppercase">{room}</h2>
 							</div>
 							{#each roomchores as chore, choreIndex}
 								<div
 									data-row-even={(choreIndex + index - 1) % 2 === 0}
-									class="flex items-center border-b border-violet-500 px-2 last:border-b-0 data-[row-even='true']:bg-violet-100"
+									class="flex items-center border-b border-sky-500 px-2 last:border-b-0 data-[row-even='true']:bg-sky-100"
 								>
 									<p class="w-full text-right text-xs font-semibold">{chore}</p>
 								</div>
@@ -110,7 +112,7 @@
 								data-last-cell-in-column={isLastBoxInColumn}
 								data-row-even={row % 2 === 0}
 								style={`grid-template-columns: repeat(${days.length}, 1fr);`}
-								class="flex flex-col items-center justify-center border-r border-b border-violet-500 px-2 text-xs data-[last-cell-in-column='true']:border-b-0 data-[last-cell-in-row='true']:border-r-0 data-[row-even='true']:bg-violet-100"
+								class="flex flex-col items-center justify-center border-r border-b border-sky-500 px-2 text-xs data-[last-cell-in-column='true']:border-b-0 data-[last-cell-in-row='true']:border-r-0 data-[row-even='true']:bg-sky-100"
 							></div>
 						{/each}
 					</div>
